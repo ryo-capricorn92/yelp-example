3c4293eb8a843cf970ba25e8fa12b6539d108443
Ryo Wheatley <mwheatle@netapp.com>
2017-04-06 18:59:21 -0400

    testing XML requests

--- script.js
+++ script.js
@@ -21,36 +21,61 @@ var clientSecret = "vB5iAAB0Q8RzO2A2gcEmO3XgSCpSoKWzzxRrzwjMDLARCtrlLHtnRDtPA6ww
 // of passing information in a query URL, we'll be giving the ajax request a data object, containing
 // all of the information that the Yelp API needs to generate our token. This includes our app ID and 
 // secret, as well as the grant type, which for now will always be "client_credentials"
-$.ajax({
-  url: "https://anyorigin.com/get?url=api.yelp.com/oauth2/token",
-  method: "POST",
-  data: {
-    client_id: clientId,
-    client_secret: clientSecret,
-    grant_type: "client_credentials"
-  },
-  dataType: "application/x-www-form-urlencoded"
-}).done(function (response) {
-  // set the global "accessToken" variable to the token that the ajax call returns
-  accessToken = response.data.access_token;
-});
+// $.ajax({
+//   url: "https://api.yelp.com/oauth2/token",
+//   method: "POST",
+//   data: {
+//     client_id: clientId,
+//     client_secret: clientSecret,
+//     grant_type: "client_credentials"
+//   },
+//   dataType: "application/x-www-form-urlencoded"
+// }).done(function (response) {
+//   // set the global "accessToken" variable to the token that the ajax call returns
+//   accessToken = response.data.access_token;
+// });
 
 // Now that we have our access token, we can make a request to the Yelp API for the data we want
 // For this example, we'll search for delis in a particular area.
 // For this example, I'll keep the variable declarations clustered with their use cases, but keep in
 // mind that you should be declaring all variables at the top of their respective scopes, wherever 
 // possible.
-// var searchTerm = "deli";
-// var latitude = "37.786882";
-// var longitude = "-122.399972";
+var searchTerm = "deli";
+// var location = "Raleigh";
 // $.ajax({
-//   url: "https://api.yelp.com/v3/businesses/search?term=" + searchTerm + "&latitude=" + latitude + 
-//     "&longitude=" + longitude,
-//   method: "GET",
-//   dataType: "jsonp"
+//   headers: {
+//     Authorization: "Bearer " + accessToken
+//   },
+//   url: "https://api.yelp.com/v3/businesses/search?term=" + searchTerm + "&location=" + "Raleigh",
+//   method: "GET"
 // }).done(function (response) {
 //   console.log(response.data);
 // });
+
+function createCORSRequest(method, url){
+    var xhr = new XMLHttpRequest();
+    if ("withCredentials" in xhr){
+        xhr.open(method, url, true);
+    } else if (typeof XDomainRequest != "undefined"){
+        xhr = new XDomainRequest();
+        xhr.open(method, url);
+    } else {
+        xhr = null;
+    }
+    xhr.setRequestHeader('Authorization', 'Bearer -3m_KBsBUolcuNlFdh4jybn1abfHckrec-NeH92SnOP49ks_EqXa1MZ9TghEWkMAYL178hb6OYUKX3APP4hfClG0PIDjPAi63scI1EoL5M-ovXxsK4tZbV2LtX_mWHYx')
+    return xhr;
+}
+
+var request = createCORSRequest("get", "https://api.yelp.com/v3/businesses/search?term=" + searchTerm + "&location=" + "Raleigh");
+if (request){
+    request.onload = function() {
+        console.log('IT\'S HAPPENING');
+    };
+    request.onreadystatechange = function (xhr, ev) {
+      console.log(xhr.response, ev);
+    };
+    request.send();
+}
 
 // The one problem that you may run into when working with this API, is that this API does not 
 // allow null host CORS requests. So you will not be able to make requests without making 
