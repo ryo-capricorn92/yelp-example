3b35bf37ed1ce3d1286ef378e613cce498094f01
Ryo Wheatley <mwheatle@netapp.com>
2017-04-06 19:15:21 -0400

    use proxy server

--- script.js
+++ script.js
@@ -21,19 +21,20 @@ var clientSecret = "vB5iAAB0Q8RzO2A2gcEmO3XgSCpSoKWzzxRrzwjMDLARCtrlLHtnRDtPA6ww
 // of passing information in a query URL, we'll be giving the ajax request a data object, containing
 // all of the information that the Yelp API needs to generate our token. This includes our app ID and 
 // secret, as well as the grant type, which for now will always be "client_credentials"
-// $.ajax({
-//   url: "https://api.yelp.com/oauth2/token",
-//   method: "POST",
-//   data: {
-//     client_id: clientId,
-//     client_secret: clientSecret,
-//     grant_type: "client_credentials"
-//   },
-//   dataType: "application/x-www-form-urlencoded"
-// }).done(function (response) {
-//   // set the global "accessToken" variable to the token that the ajax call returns
-//   accessToken = response.data.access_token;
-// });
+$.ajax({
+  url: "https://thingproxy.freeboard.io/fetch/https://api.yelp.com/oauth2/token",
+  method: "POST",
+  data: {
+    client_id: clientId,
+    client_secret: clientSecret,
+    grant_type: "client_credentials"
+  },
+  dataType: "application/x-www-form-urlencoded"
+}).done(function (response) {
+  // set the global "accessToken" variable to the token that the ajax call returns
+  console.log(response.data.access_token);
+  accessToken = response.access_token;
+});
 
 // Now that we have our access token, we can make a request to the Yelp API for the data we want
 // For this example, we'll search for delis in a particular area.
@@ -46,36 +47,36 @@ var searchTerm = "deli";
 //   headers: {
 //     Authorization: "Bearer " + accessToken
 //   },
-//   url: "https://api.yelp.com/v3/businesses/search?term=" + searchTerm + "&location=" + "Raleigh",
+//   url: "https://thingproxy.freeboard.io/fetch/https://api.yelp.com/v3/businesses/search?term=" + searchTerm + "&location=" + "Raleigh",
 //   method: "GET"
 // }).done(function (response) {
-//   console.log(response.data);
+//   console.log(response);
 // });
 
-function createCORSRequest(method, url){
-    var xhr = new XMLHttpRequest();
-    if ("withCredentials" in xhr){
-        xhr.open(method, url, true);
-    } else if (typeof XDomainRequest != "undefined"){
-        xhr = new XDomainRequest();
-        xhr.open(method, url);
-    } else {
-        xhr = null;
-    }
-    xhr.setRequestHeader('Authorization', 'Bearer -3m_KBsBUolcuNlFdh4jybn1abfHckrec-NeH92SnOP49ks_EqXa1MZ9TghEWkMAYL178hb6OYUKX3APP4hfClG0PIDjPAi63scI1EoL5M-ovXxsK4tZbV2LtX_mWHYx')
-    return xhr;
-}
+// function createCORSRequest(method, url){
+//     var xhr = new XMLHttpRequest();
+//     if ("withCredentials" in xhr){
+//         xhr.open(method, url, true);
+//     } else if (typeof XDomainRequest != "undefined"){
+//         xhr = new XDomainRequest();
+//         xhr.open(method, url);
+//     } else {
+//         xhr = null;
+//     }
+//     xhr.setRequestHeader('Authorization', 'Bearer -3m_KBsBUolcuNlFdh4jybn1abfHckrec-NeH92SnOP49ks_EqXa1MZ9TghEWkMAYL178hb6OYUKX3APP4hfClG0PIDjPAi63scI1EoL5M-ovXxsK4tZbV2LtX_mWHYx')
+//     return xhr;
+// }
 
-var request = createCORSRequest("get", "https://api.yelp.com/v3/businesses/search?term=" + searchTerm + "&location=" + "Raleigh");
-if (request){
-    request.onload = function() {
-        console.log('IT\'S HAPPENING');
-    };
-    request.onreadystatechange = function (xhr, ev) {
-      console.log(xhr.response, ev);
-    };
-    request.send();
-}
+// var request = createCORSRequest("get", "https://api.yelp.com/v3/businesses/search?term=" + searchTerm + "&location=" + "Raleigh");
+// if (request){
+//     request.onload = function() {
+//         console.log('IT\'S HAPPENING');
+//     };
+//     request.onreadystatechange = function (xhr, ev) {
+//       console.log(xhr.response, ev);
+//     };
+//     request.send();
+// }
 
 // The one problem that you may run into when working with this API, is that this API does not 
 // allow null host CORS requests. So you will not be able to make requests without making 
